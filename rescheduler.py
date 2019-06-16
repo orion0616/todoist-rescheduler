0513f97a491e8dbcfbfbb5babacb872c69825eaf
orion46 <earth.nobu.light@gmail.com>
2019-06-16 23:06:26 +0900

    Adapt to API v8

--- rescheduler.py
+++ rescheduler.py
@@ -2,27 +2,25 @@ import os
 import sys
 import datetime
 
-from todoist.api import TodoistAPI
+import todoist
 from exlist import ExList
 
 def missDeadLine(item):
-    due = item['due_date_utc']
-    due_datetime = datetime.datetime.strptime(due, "%a %d %b %Y %H:%M:%S %z")
-    now = datetime.datetime.now(datetime.timezone.utc)
+    due = item['due']['date']
+    date = due + " 23:59:59"
+    due_datetime = datetime.datetime.strptime(date, "%Y-%m-%d %H:%M:%S")
+    now = datetime.datetime.now()
     return due_datetime < now
 
 def hasDeadLine(item):
-    return item['date_string'] is not None
+    return item.data['due'] is not None
 
-def makeProjectMap(api):
-    projects = ExList(api.state['projects']).map(lambda p: (p['id'], p['name']))
-    return dict(projects)
-
-def resche1DayTask(api, item):
-    now = datetime.datetime.utcnow()
-    due = now + datetime.timedelta(hours=9)
-    due = due.strftime('%-m月%-d日')
-    item.update(date_string= due +'から毎日')
+def reschedule(api, item):
+    content = {'string' : item.data['due']['string']}
+    item.update(due=None)
+    api.commit()
+    item.update(due=content)
+    api.commit()
 
 def main():
     key = os.getenv("TODOIST_TOKEN")
@@ -30,17 +28,12 @@ def main():
         print("Environment Variable named $TODOIST_TOKEN doesn't exist!")
         sys.exit()
 
-    api = TodoistAPI(key, cache=None)
+    api = todoist.TodoistAPI(key)
     api.sync()
     items = ExList(api.state['items'])\
             .filter(hasDeadLine)\
             .filter(missDeadLine)
-    projectMap = makeProjectMap(api)
-
-    # For 1day routine
-    items.filter(lambda i: projectMap[i['project_id']] == '1Day Routine')\
-            .foreach(lambda i: resche1DayTask(api, i))
-    api.commit()
+    items.foreach(lambda item: reschedule(api, item))
 
 def exe(event, context):
     main()
